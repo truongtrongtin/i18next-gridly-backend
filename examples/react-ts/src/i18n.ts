8e01f2c0177dd91fd34c66b1240881647e776020
Tin Truong <truongtrongtin0305@gmail.com>
2023-01-07 22:19:06 +0700

    chore(examples): use env variable

--- examples/react-ts/src/i18n.ts
+++ examples/react-ts/src/i18n.ts
@@ -3,16 +3,16 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import GridlyBackend, { GridlyBackendOptions } from 'i18next-gridly-backend';
 import { initReactI18next } from 'react-i18next';
 
+const isProduction = import.meta.env.PROD;
+
 const backendOptions: GridlyBackendOptions = {
   apiKey: import.meta.env.VITE_API_KEY,
   viewId: import.meta.env.VITE_VIEW_ID,
 };
 
-const isProduction = import.meta.env.NODE_ENV === 'production';
-
 i18n
-  .use(GridlyBackend)
   .use(LanguageDetector)
+  .use(GridlyBackend)
   .use(initReactI18next)
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
